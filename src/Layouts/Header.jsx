08c89f24eaf882239831f021606c057ba65f312c
9BankWasan <wasan.serm019@gmail.com>
2025-11-16 16:30:31 +0700

    Refactor Header component to remove user display and improve logo import

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -1,20 +1,11 @@
-function Header({username, Signout}) {
+import NotepadLogo from '../assets/notepad.svg';
+
+function Header() {
   return (
     <section className='header'>
-      <div className='bg-slate-600 flex flex-row px-12 py-4 items-center justify-between'>
-        <div className="flex gap-4">
-          <img src="./src/assets/notepad.svg" alt="notepad" className="size-10"/>
-          <h1 className='text-4xl font-semibold text-white'>Task List</h1>
-        </div>
-        {username && (
-          <div>
-            <p>User: {username}</p>
-            <button type='button' onClick={Signout} className='flex gap-2 text-white'>
-              <span>Sign out</span>
-              <span className='material-icons'>logout</span>
-            </button>
-          </div>
-        )}
+      <div className='bg-slate-600 flex flex-row px-12 py-4 gap-2 items-center'>
+        <img src={NotepadLogo} alt="notepad" className="size-10"/>
+        <h1 className='text-4xl font-semibold text-white'>Task List</h1>
       </div>
     </section>
   );
